68309e5b180813bcdac939d4f70d8c968a30dbf7
Christian Roy <croy@cimpress.com>
2018-12-07 17:38:25 -0800

    start refactoring day 7 to jobs and workers

--- src/day7.test.ts
+++ src/day7.test.ts
@@ -1,6 +1,38 @@
 import _ from 'lodash';
 import readInput from './readInput';
 
+type Step = {
+  id: string,
+  timeToComplete: number,
+  dependencies: string[],
+}
+
+type Job = {
+  completionTime: number,
+  step: Step,
+}
+
+function buildSteps(inputs: {step: string, depends: string}[]): Step[] {
+  const dependencyMap = _.chain(inputs)
+    .groupBy(dep => dep.step)
+    .mapValues(deps => _.flatMap(deps, dep => dep.depends))
+    .value();
+  const allSteps = _.chain(
+      _.concat(
+        _.map(inputs, i => i.step),
+        _.map(inputs, i => i.depends)))
+      .sort()
+      .uniq()
+      .value();
+  return _.chain(allSteps)
+    .map(step => ({
+      id: step,
+      timeToComplete: 61 + (step.codePointAt(0) as number) - ('A'.codePointAt(0) as number),
+      dependencies: dependencyMap[step] || [],
+    }))
+    .value();
+}
+
 function parseLine(line: string) {
   const regex = /Step (.) must be finished before step (.) can begin./;
   const matches = regex.exec(line);
@@ -13,39 +45,41 @@ function parseLine(line: string) {
   throw 'beep boop';
 }
 
-function part1() {
-  const input = readInput('day7', parseLine);
-  const dependencyMap = _.chain(input) 
-    .groupBy(dep => dep.step)
-    .mapValues(deps => _.flatMap(deps, dep => dep.depends))
-    .value();
-  const allSteps = _.chain(
-      _.concat(
-        _.map(input, i => i.step),
-        _.map(input, i => i.depends)))
-      .sort()
-      .uniq()
-      .value();
-  const completionOrder = [];
-  const completed = new Set();
-  while (completionOrder.length !== allSteps.length) {
-    const next = _.chain(allSteps)
-      .filter(step => !completed.has(step))
-      .find(step => _.every(dependencyMap[step], dep => completed.has(dep)))
-      .value();
-    completed.add(next);
-    completionOrder.push(next);
+function completeSteps(steps: Step[], workers: number): Job[] {
+  const stepById = _.keyBy(steps, step => step.id);
+  const allStepIds = _.keys(stepById).sort();
+  const completionOrder = [] as Job[];
+  const completedStepIds = new Set();
+  while (completionOrder.length !== allStepIds.length) {
+    const next = _.chain(allStepIds)
+      .filter(step => !completedStepIds.has(step))
+      .find(step => _.every(stepById[step].dependencies, dep => completedStepIds.has(dep)))
+      .value() as string;
+    completedStepIds.add(next);
+    completionOrder.push({
+      step: stepById[next],
+      completionTime: 0,
+    });
   }
-  return _.join(completionOrder, '');
+  return completionOrder;
 }
 
+function part1() {
+  const input = readInput('day7', parseLine);
+  const steps = buildSteps(input);
+  return _.chain(completeSteps(steps, 1))
+    .map(job => job.step.id)
+    .join('')
+    .value();
+}
 
 function part2() {
 }
 
 describe('output', () => {
   test('part 1 ', () => {
-    console.log(part1());
+    const result = part1();
+    expect(result).toBe('CFMNLOAHRKPTWBJSYZVGUQXIDE')
   })
 
   test('part 2 ', () => {
